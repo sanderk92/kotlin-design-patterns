289c840c04888df0cb6d981e2e99dbc0acc255f8
Sander <sanderkrabbenborg@hotmail.com>
2023-02-19 16:46:35 +0100

    Add ChainOfResponsibility pattern

---
+++ src/main/kotlin/com/example/patterns/creational/chainofresponsibility/ChainOfResponsibility.kt
@@ -0,0 +1,37 @@
+package com.example.patterns.creational.chainofresponsibility
+
+data class Request(
+    val value: String,
+)
+
+data class Result(
+    val message: String,
+)
+
+interface Handler {
+    val next: Handler?
+    fun process(request: Request): Result
+}
+
+class ValidationHandler(override val next: Handler? = null) : Handler {
+    override fun process(request: Request): Result {
+        if (request.value.isEmpty()) {
+            return Result("Empty value!")
+        }
+        println("Validated request!")
+        return next?.process(request) ?: Result("Validation succeeded")
+    }
+}
+
+class CachingHandler(override val next: Handler? = null) : Handler {
+    override fun process(request: Request): Result {
+        println("Cached request!")
+        return next?.process(request) ?: Result("Caching succeeded")
+    }
+}
+
+fun main() {
+    val cachingHandler = CachingHandler()
+    val validationHandler = ValidationHandler(cachingHandler)
+    validationHandler.process(Request("Test"))
+}
